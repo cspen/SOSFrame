93e81046c064de709717a4e282178ae7b4119629
unknown <cspen@modintro.com>
2018-07-13 22:29:06 -0500

    Implement logic for search by url

--- Model/SOSModel.php
+++ Model/SOSModel.php
@@ -10,7 +10,24 @@ class SOSModel {
 	}
 	
 	public function updateState($value) {
-		// Database query goes here
+		$path = str_replace("/Ozone/SOSFrame/Public/", "", $_SERVER['REQUEST_URI']);
+		$request = explode("/", $path);
+		
+		if(is_array($request)) {
+			$c = count($request);
+			if($c >= 2 && $request[1] != "") {
+				// Article page
+				echo '<br> Article page - '.$request[1];
+				// Do database search based on article title
+			} else if($c <= 2) {
+				// Topic page
+				echo '<br> Topic page - '.$request[0];
+				// Do database search based on topic
+			}
+		}
+		
+		
+		/*
 		$output = new SOSOutput(
 				$value,
 				"This is the description",
@@ -18,6 +35,7 @@ class SOSModel {
 				"This is the Content Body",
 				"Prev content",
 				"Next content");
-		$this->view->respond($output);		
+		$this->view->respond($output);	
+		*/	
 	}
 }

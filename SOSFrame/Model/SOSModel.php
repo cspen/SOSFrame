87256783f4c4ae24b789216301352cac96a26280
unknown <cspen@modintro.com>
2018-07-31 02:07:28 -0500

    Fix navigation problem

--- SOSFrame/Model/SOSModel.php
+++ SOSFrame/Model/SOSModel.php
@@ -11,58 +11,50 @@ class SOSModel {
 	}
 	
 	public function updateState($value) {
-		$path = str_ireplace("/Ozone/SOSFrame/Public/", "", $_SERVER['REQUEST_URI']);
-		$request = explode("/", $path);
-		
-		print_r($request);
+		$requestURI = explode("?", $_SERVER['REQUEST_URI']);
+		$requestURI = $requestURI[0];
+		$params = explode("/", $_SERVER['REQUEST_URI']);
 		
 		$db = new DBConnection();
 		$dbconn = $db->getConnection();
 		$query = "";
-		if(is_array($request)) {			
-			$c = count($request);
-			echo "<br>COUNT ".$c;
-			if(isset($request[1]) && $request[1] != "") {
-				// Article page
-				$output = new SOSOutput(
-						"Article",
-						"This is the description",
-						"This is the Content Title",
-						"This is the Content Body",
-						"Prev content",
-						"Next content");
-				$this->view->articlePage($output);
-			} else if($request[0] != "") {
-				if(isset($request[1]) && $request[1] == "") {
-					// Topic page				
-					// $query = "SELECT * FROM article WHERE topic=:topic";
-					// $stmt->bindParam(':topic', $request[0]);
-					$output = new SOSOutput(
-						"Topic",
-						"This is the description",
-						"This is the Content Title",
-						"This is the Content Body",
-						"Prev content",
-						"Next content");
-					$this->view->topicPage($output);
-				} else {
-					header('HTTP/1.1 301 Moved Permanently');
-					header('Location: http://'.$_SERVER['SERVER_NAME'].'/'.$_SERVER['REQUEST_URI'].'/');
-				}
-			} else if($request[0] == "") {
-				$output = new SOSOutput(
-						"Science of Stupidity",
-						"This is the description",
-						"This is the Content Title",
-						"This is the Content Body",
-						"Prev content",
-						"Next content");
-				$this->view->homePage($output);
-			} else {
-				echo '404 NOT FOUND';
-				// echo 'ELSE';
-			}
+		if(preg_match('/^\/Ozone\/SOSFrame\/Public\/([A-za-z0-9-]+)\/([A-za-z0-9-]+)$/', $requestURI)) {
+			// Article page
+			$output = new SOSOutput(
+				"Article",
+				"This is the description",
+				"This is the Content Title",
+				"This is the Content Body",
+				"Prev content",
+				"Next content");
+			$this->view->articlePage($output);
+		} else if(preg_match('/^\/Ozone\/SOSFrame\/Public\/([A-Za-z0-9-]+)\/$/', $requestURI)) {
+			// Topic page				
+			// $query = "SELECT * FROM article WHERE topic=:topic";
+			// $stmt->bindParam(':topic', $request[0]);
+			$output = new SOSOutput(
+				"Topic",
+				"This is the description",
+				"Topic Content Title",
+				"Topic Content Body",
+				"Prev content",
+				"Next content");
+			$this->view->topicPage($output);
+		} else if(preg_match('/^\/Ozone\/SOSFrame\/Public\/$/', $requestURI)) {
+			$output = new SOSOutput(
+				"Science of Stupidity",
+				"This is the description",
+				"Home Page Content Title",
+				"Home Page Content Body",
+				"Prev content",
+				"Next content");
+			$this->view->homePage($output);
+		} else {
+			header('HTTP/1.1 404 Not Found');
+			// Need custom 404 page
+			echo '404 NOT FOUND';
 		}
+		
 		
 		/*
 		$stmt = $dbconn->prepare($query);
@@ -70,12 +62,6 @@ class SOSModel {
 			$rowCount = $stmt->rowCount();
 			if($rowCount == 1) {
 				$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		*/
-		
-		
-		
-		
-			
-		
+		*/		
 	}
 }

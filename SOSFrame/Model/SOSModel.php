d47e1548ba2844f9542c6ef7c178af726d885986
unknown <cspen@modintro.com>
2018-08-01 07:19:05 -0500

    Add output functions

--- SOSFrame/Model/SOSModel.php
+++ SOSFrame/Model/SOSModel.php
@@ -20,41 +20,25 @@ class SOSModel {
 		$query = "";
 		if(preg_match('/^\/Ozone\/SOSFrame\/Public\/([A-za-z0-9-]+)\/([A-za-z0-9-]+)$/', $requestURI)) {
 			// Article page
-			$output = new SOSOutput(
-				"Article",
-				"This is the description",
-				"This is the Content Title",
-				"This is the Content Body",
-				"Prev content",
-				"Next content");
+			$title = end($params);
+			$topic = prev($params);			
+			$output = $this->getArticle($title, $topic);
 			$this->view->articlePage($output);
 		} else if(preg_match('/^\/Ozone\/SOSFrame\/Public\/([A-Za-z0-9-]+)\/$/', $requestURI)) {
 			// Topic page				
 			// $query = "SELECT * FROM article WHERE topic=:topic";
 			// $stmt->bindParam(':topic', $request[0]);
-			$output = new SOSOutput(
-				"Topic",
-				"This is the description",
-				"Topic Content Title",
-				"Topic Content Body",
-				"Prev content",
-				"Next content");
+			$topic = end($params);
+			$output = $this->getTopic($topic);
 			$this->view->topicPage($output);
 		} else if(preg_match('/^\/Ozone\/SOSFrame\/Public\/$/', $requestURI)) {
-			$output = new SOSOutput(
-				"Science of Stupidity",
-				"This is the description",
-				"Home Page Content Title",
-				"Home Page Content Body",
-				"Prev content",
-				"Next content");
+			$output = $this->getHome();
 			$this->view->homePage($output);
 		} else {
 			header('HTTP/1.1 404 Not Found');
 			// Need custom 404 page
 			echo '404 NOT FOUND';
-		}
-		
+		}		
 		
 		/*
 		$stmt = $dbconn->prepare($query);
@@ -63,5 +47,47 @@ class SOSModel {
 			if($rowCount == 1) {
 				$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		*/		
+	}
+	
+	private function getArticle($title, $topic) {
+		return new SOSOutput(
+				$title,
+				"This is the description",
+				"This is the Content Title",
+				"This is the Content Body",
+				$this->getMenu(),
+				"Prev content",
+				"Next content");
+	}
+	
+	private function getTopic($topic) {
+		return new SOSOutput(
+				$topic,
+				"This is the description",
+				"Topic Content Title",
+				"Topic Content Body",
+				$this->getMenu(),
+				"Prev content",
+				"Next content");
+	}
+	
+	private function getHome() {
+		return new SOSOutput(
+				"Science of Stupidity",
+				"This is the description",
+				"Home Page Content Title",
+				"Home Page Content Body",
+				$this->getMenu(),
+				"Prev content",
+				"Next content");
+	}
+	
+	private function getMenu() {
+		// TO-DO: Make db query for topics and pass
+		// to view. (View should create html)
+		return '<p><a href="javascript:void(0)">Health &amp; Fitness</a></p>
+			<p><a href="javascript:void(0)">Photography</a></p>
+			<p><a href="javascript:void(0)">Illustrator</a></p>
+			<p><a href="javascript:void(0)">Media</a></p>';
 	}
 }

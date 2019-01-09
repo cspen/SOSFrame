396a5424c8c3c31778c5f11faf4889c2e5c8feee
unknown <cspen@modintro.com>
2019-01-08 23:53:18 -0600

    Correct path query

--- SOSFrame/Model/DBQueries.php
+++ SOSFrame/Model/DBQueries.php
@@ -7,7 +7,7 @@
 
 interface DBQueries {
 	
-	const PATH_QUERY = 'SELECT * FROM article WHERE path=:path';
+	const PATH_QUERY = 'SELECT * FROM article WHERE article_path=:path';
 	
 	const TOPIC_QUERY = 'SELECT * FROM article WHERE topic=:topic';
 	
@@ -16,7 +16,7 @@ interface DBQueries {
 	const LOGIN_QUERY = 'SELECT password FROM user WHERE name=:name';
 	
 	const TOPIC_MENU_QUERY =
-			'SELECT DISTINCT SUBSTRING_INDEX(article_path, "/", 1) FROM article';
+			'SELECT DISTINCT SUBSTRING_INDEX(article_path, "/", 1) AS topic FROM article';
 }
 
 ?>

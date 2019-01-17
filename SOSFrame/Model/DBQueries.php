64120db5d015f576399edf4b81d4f301c7f31dfa
unknown <cspen@modintro.com>
2019-01-16 23:19:35 -0600

    Add topic query

--- SOSFrame/Model/DBQueries.php
+++ SOSFrame/Model/DBQueries.php
@@ -7,9 +7,16 @@
 
 interface DBQueries {
 	
-	const PATH_QUERY = 'SELECT * FROM article WHERE article_path=:path';
+	const PATH_QUERY = 'SELECT article_title, article_description,
+			article_body, article_publish_date, article_parent, etag,
+			article.last_modified, user_first_name, user_last_name
+			FROM article, user, author
+			WHERE author.articleID = article.articleID
+			AND author.userID = user.userID
+			AND article.article_path = :path';
 	
-	const TOPIC_QUERY = 'SELECT * FROM article WHERE topic=:topic';
+	const TOPIC_QUERY = 'SELECT article_title FROM article WHERE 
+			article_path LIKE CONCAT(:path, "%")';
 	
 	const HOME_QUERY = '';
 	

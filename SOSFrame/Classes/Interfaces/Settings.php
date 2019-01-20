c8948523da2694dfe5bbb946b43d4aff6a9b5f75
unknown <cspen@modintro.com>
2019-01-19 23:48:37 -0600

    Implement Settings interface

---
+++ SOSFrame/Classes/Interfaces/Settings.php
@@ -0,0 +1,14 @@
+<?php
+
+interface Settings {
+	
+	const APP_URL = "/Ozone/SOSFrame/Public/";
+	
+	const HOME_PAGE_DESCRIPTION = "Better living through trial and error";
+	const HOME_PAGE_TITLE = "Home Page Title";
+	
+	const SITE_TITLE = "Science of Stupidity";
+	
+}
+
+?>

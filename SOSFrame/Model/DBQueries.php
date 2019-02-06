5e2d6a55e0861cd218f4ec915c9081204729dd06
unknown <cspen@modintro.com>
2019-02-06 02:17:34 -0600

    Change topic_query

--- SOSFrame/Model/DBQueries.php
+++ SOSFrame/Model/DBQueries.php
@@ -15,7 +15,7 @@ interface DBQueries {
 			AND author.userID = user.userID
 			AND article.article_path = :path';
 	
-	const TOPIC_QUERY = 'SELECT article_title FROM article WHERE 
+	const TOPIC_QUERY = 'SELECT article_path FROM article WHERE 
 			article_path LIKE CONCAT(:path, "%")';
 	
 	const HOME_QUERY = 'SELECT * FROM article ORDER BY article_publish_date
@@ -25,7 +25,8 @@ interface DBQueries {
 			user_first_name=:fname AND user_last_name=:lname';
 	
 	const TOPIC_MENU_QUERY =
-			'SELECT DISTINCT SUBSTRING_INDEX(article_path, "/", 1) AS topic FROM article';
+			'SELECT DISTINCT SUBSTRING_INDEX(article_path, "/", 1) AS
+			topic FROM article';
 }
 
 ?>

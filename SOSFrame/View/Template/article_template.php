0e5a8d8593ec9e515cbd6aca4ff4bff8a5192cfc
unknown <cspen@modintro.com>
2019-02-21 00:00:49 -0600

    Add navigation menu

--- SOSFrame/View/Template/article_template.php
+++ SOSFrame/View/Template/article_template.php
@@ -2,6 +2,8 @@
 require_once('../SOSFrame/Classes/Interfaces/Settings.php');
 
 $styleSheet = Settings::APP_URL."styles/template.css";
+$siteTitle = Settings::SITE_TITLE;
+$siteURL = Settings::APP_URL;
 
 $html = <<< EOT
 <!DOCTYPE html>
@@ -54,6 +56,7 @@ $sideMenu;
 <div class="w3-twothird">
 
 <div class="w3-container w3-card w3-white w3-margin-bottom">
+<small>$navLink</small>
 <h2 class="w3-text-grey w3-padding-16">$contentTitle</h2>
 <div class="w3-container">
 <h5 class="w3-opacity"><b>$author / $publishDate</b></h5>

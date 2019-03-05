b6b06f38dcd3ba5fea8d16c719bdbb108ada6920
unknown <cspen@modintro.com>
2019-03-04 22:55:52 -0600

    Implement constructor12

--- SOSFrame/Classes/SOSArticleOutput.php
+++ SOSFrame/Classes/SOSArticleOutput.php
@@ -1,6 +1,6 @@
 <?php
 require_once('SOSOutput.php');
-require_once('SOSArticleOutput.php');
+// require_once('SOSArticleOutput.php');
 
 class SOSArticleOutput extends SOSOutput {
 	
@@ -9,7 +9,7 @@ class SOSArticleOutput extends SOSOutput {
 	private $contentNext;
 	private $contentPrev;
 	
-	function __construct() {
+	function __construct() { 
 		$args = func_get_args();
 		$num = func_num_args();
 		if (method_exists($this,$f='__construct'.$num)) {
@@ -17,17 +17,27 @@ class SOSArticleOutput extends SOSOutput {
 		}
 	}
 	
-	function __construct9($pageTitle, $description, $contentTitle,
-			$contentBody, $topicsMenu, $contentPrev, $contentNext,
+	function __construct10($pageTitle, $description, $contentTitle,
+			$contentBody, $sideMenuTitle, $sideMenu, $contentPrev, $contentNext,
 			$author, $publishDate) {
-		
 			$this->author = $author;
 			$this->publishDate = $publishDate;
 			$this->contentPrev = $contentPrev;
 			$this->contentNext = $contentNext;
 			
 			parent::__construct($pageTitle, $description, $contentTitle,
-					$contentBody, $topicsMenu);		
+					$contentBody, $sideMenuTitle, $sideMenu);		
+	}
+	
+	function __construct12($pageTitle, $description, $contentTitle,
+			$contentBody, $sideMenuTitle, $sideMenu, $contentPrev,
+			$contextNext, $author, $publishDate, $etag, $lastMod) {
+			$this->etag = $etag;
+			$this->lastMod = $lastMod;
+			
+			parent::__constuct10($pageTitle, $description, $contentTitle,
+					$contentBody, $sideMenuTitle, $sideMenu, $contentPrev, $contentNext,
+					$author, $publishDate);
 	}
 	
 	public function author() {

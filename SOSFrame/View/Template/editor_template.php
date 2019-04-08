9a757f959651511537d440f3a72b96c9791289e1
unknown <cspen@modintro.com>
2019-04-07 21:17:17 -0500

    Style editor

--- SOSFrame/View/Template/editor_template.php
+++ SOSFrame/View/Template/editor_template.php
@@ -1,7 +1,7 @@
 <?php
 require_once('../SOSFrame/Classes/Interfaces/Settings.php');
 
-$styleSheet = Settings::APP_URL."styles/template.css";
+$styleSheet = Settings::APP_URL."styles/editor.css";
 $siteTitle = Settings::SITE_TITLE;
 $siteURL = Settings::APP_URL;
 
@@ -19,73 +19,71 @@ $html = <<< EOT
 <body class="w3-light-grey">
 
 <!-- Page Container -->
-<div class="w3-content w3-margin-top" style="max-width:1400px;">
+<div class="w3-content w3-margin-top">
 
-<!-- The Grid -->
-<div class="w3-row-padding">
+	<!-- The Grid -->
+	<div class="w3-row-padding">
 
-<!-- Left Column -->
-<div class="w3-third">
-<div class="w3-white w3-text-grey w3-card-4">
-<div class="w3-display-container">
-<div class="w3-container w3-text-black">
-<h2 id="blogtitle">Science of Stupidity</h2>
-<small id="tagline">Better living through trial and error</small><hr>
-</div>
-</div>
-<div class="w3-container">
-<p><a href="javascript:void(0)">Contact</a></p>
-<p><a href="javascript:void(0)">About</a></p>
-<hr>
+		<!-- Left Column -->
+		<div class="w3-third">
+			<div class="w3-white w3-text-grey w3-card-4">
 
-<p class="w3-large">
-	<b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>
-	$sideMenuTitle</b>
-</p>
-$sideMenu
-<br>
-</div>
-</div><br>
+				<div class="w3-display-container">
+					<div class="w3-container w3-text-black">
+						<h2 id="blogtitle">Science of Stupidity</h2>
+						<small id="tagline">Better living through trial and error</small><hr>
+					</div>
+				</div>
 
-<!-- End Left Column -->
+				<p class="w3-large">
+					<b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>
+					$sideMenuTitle</b>
+				</p>
+				$sideMenu
+				<br>
+			</div>
+		</div>
+		<br>
+		<!-- End Left Column -->
 </div>
 
 <!-- Right Column -->
 <div class="w3-twothird">
 
 <div class="w3-container w3-card w3-white w3-margin-bottom">
-<h2 class="w3-text-grey w3-padding-16">Editor</h2>
-<select>
-	<option>Title of Draft 1</option>
-	<option>Title of Draft 2</option>
-</select>
-<div class="w3-container">
-<label for="title">Title: <input type="text" name="title" id="title"></label>
-<br>
-<label for="body">Content:
-<textarea rows="20" style="font-size: xx-large;"></textarea></label>
-<hr>
-<label for="description">Description: <input type="text" id="description"></label>
+	<h2 class="w3-text-grey w3-padding-16">Editor</h2>
+		<select>
+			<option>Title of Draft 1</option>
+			<option>Title of Draft 2</option>
+		</select>
 
-<label for="topiclist">Topic:
-<input list="topics" id="topiclist" name="topiclist"></label>
-<datalist id="topics" name="topics">
-  <!-- Need to make a list of topics -->
-  <option value="Internet Explorer">
-  <option value="Firefox">
-  <option value="Chrome">
-  <option value="Opera">
-  <option value="Safari">
-</datalist>
-<br><br>
+	<div class="w3-container">
+		<label for="title">Title: <input type="text" name="title" id="title"></label>
+		<br>
+		<label for="content">Content:</label>
+		<textarea rows="20" style="" id="content"></textarea>
+		<hr>
+		<label for="description">Description: <input type="text" id="description"></label>
 
-<select>
-	<option>Draft</option>
-	<option>Publish</option>
-</select>
-<button type="button">Update</button>
+		<label for="topiclist">Topic:
+		<input list="topics" id="topiclist" name="topiclist"></label>
+		<datalist id="topics" name="topics">
+  			<!-- Need to make a list of topics -->
+  				<option value="Internet Explorer">
+  				<option value="Firefox">
+  				<option value="Chrome">
+  				<option value="Opera">
+  				<option value="Safari">
+			</datalist>
+			<br><br>
 
-</div>
+		<select>
+			<option>Draft</option>
+			<option>Publish</option>
+		</select>
+		<button type="button">Update</button>
+
+	</div>
 
 
 <!-- End Right Column -->

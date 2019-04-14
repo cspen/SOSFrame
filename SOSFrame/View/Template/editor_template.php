f6b70bb66d2b3c61e3c160ae41b29f51b757574e
unknown <cspen@modintro.com>
2019-04-13 23:52:56 -0500

    Style main form

--- SOSFrame/View/Template/editor_template.php
+++ SOSFrame/View/Template/editor_template.php
@@ -28,7 +28,7 @@ $html = <<< EOT
 		<div class="">
 			<a>Science of Stupidity</a>	
 			<hr>
-			<ul id="navlist">
+			<ul class="navlist">
 				<li style="padding: 0;"><button>Sign out</button></li>	
 				<li style="float: left;"><button>New</button><li>
 				<li style="float: left;">
@@ -47,40 +47,39 @@ $html = <<< EOT
 		<!-- Right Column -->
 		<div class="w3-twothird">
 
-			<div class="">
-			<form action="javascript:void(0);" method="POST">
+			<form action="javascript:void(0);" method="POST" style="border: 1px solid red;">
+				<label for="title">Title: </label>
+				<input type="text" name="title" id="title" class="inline-input">
+				<br>
 				
+				<label for="content">Content: </label>
+				<textarea rows="20" style="" id="content"></textarea>
+				<hr>
+				<label for="description">Description: </label>
+				<input type="text" id="description" class="inline-input">
 
-				<div class="w3-container">
-					<label for="title">Title: </label>
-					<input type="text" name="title" id="title" class="inline-input">
-					<br>
-					<label for="content">Content: </label>
-					<textarea rows="20" style="" id="content"></textarea>
-					<hr>
-					<label for="description">Description: </label>
-					<input type="text" id="description" class="inline-input">
-
-					<label for="topiclist">Topic: </label>
-					<input list="topics" id="topiclist" name="topiclist" class="inline-input">
-					<datalist id="topics" name="topics">
-  						<!-- Need to make a list of topics -->
-  						<option value="Internet Explorer">
-  						<option value="Firefox">
-  						<option value="Chrome">
-  						<option value="Opera">
-  						<option value="Safari">
-					</datalist>
-					<br>
-					<br>
-
-					<select>
-						<option>Draft</option>
-						<option>Publish</option>
-					</select>
-					<input type="submit" value="Update">
+				<label for="topiclist">Topic: </label>
+				<input list="topics" id="topiclist" name="topiclist" class="inline-input">
+				<datalist id="topics" name="topics">
+  					<!-- Need to make a list of topics -->
+  					<option value="Internet Explorer">
+  					<option value="Firefox">
+  					<option value="Chrome">
+  					<option value="Opera">
+  					<option value="Safari">
+				</datalist>
+				
+				<ul class="navlist" style="border: 1px solid blue">
+					<li>
+						<select>
+							<option>Draft</option>
+							<option>Publish</option>
+						</select>
+					</li>
+					<li><input type="submit" value="Update"></li>
+				</ul>
 			</form>
-			</div>
+			
 		<!-- End Right Column -->
 		</div>
 

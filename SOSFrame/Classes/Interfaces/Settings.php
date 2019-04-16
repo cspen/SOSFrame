10ea47e02e295a5a641c2e1c81d96a98b41c957e
unknown <cspen@modintro.com>
2019-04-15 22:46:00 -0500

    Change HOME_PAGE_TITLE

--- SOSFrame/Classes/Interfaces/Settings.php
+++ SOSFrame/Classes/Interfaces/Settings.php
@@ -2,12 +2,18 @@
 
 interface Settings {
 	
+	// Home page url
 	const APP_URL = "/Ozone/SOSFrame/Public/";
 	
+	// Home page meta description and page title
 	const HOME_PAGE_DESCRIPTION = "Better living through trial and error";
-	const HOME_PAGE_TITLE = "Home Page Title";
+	const HOME_PAGE_TITLE = "Science of Stupidity";
 	
+	// Name of site
 	const SITE_TITLE = "Science of Stupidity";
+	
+	// Admin login page location
+	const ADMIN_LOGIN_PAGE = "secret";
 	
 }
 
